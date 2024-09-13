baeee30d6a6356caa9ba3208cf9e1aceba129124
JUHWAN OH <calculus9006@gmail.com>
2024-09-14 00:38:48 +0900

    Title: 데이터 교환, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

--- 240914/데이터 교환/exchange-data.java
+++ 240914/데이터 교환/exchange-data.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a = 5, b = 6, c = 7;
-        int tempA, tempB;
-        tempB = b;
-        b = a;
-        tempA = a;
+        
+        int temp = a;
         a = c;
-        c = tempB;
+        c = b;
+        b = temp;
+
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
