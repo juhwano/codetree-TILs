d63628ace4cf65e4b1e4d2f20ea70d0e67a8ecfd
JUHWAN OH <calculus9006@gmail.com>
2024-09-16 09:49:56 +0900

    Title: 윤년인가, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240916/윤년인가/is-leap-year.java
+++ 240916/윤년인가/is-leap-year.java
@@ -5,8 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         // 윤년 조건 (4로 나누어 떨어짐, 100으로 나누어 떨어지지 않고 400으로 나누어 떨어지는 해)
         int y = sc.nextInt();
-        if ((y % 4 == 0) || (y % 100 != 0 && y % 400 == 0)){
-            System.out.println("true");
+        if (y % 4 == 0 ){
+            if(y % 100 == 0 && y % 400 != 0){
+                System.out.println("false");
+            }else{
+                System.out.println("true");
+            }
         }else{ 
             System.out.println("false");
         }
