d2716fab7a92ff98757d35a960ba61ecc68427f1
JUHWAN OH <calculus9006@gmail.com>
2024-09-17 01:22:20 +0900

    Title: 소수 판별기, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

--- 240917/소수 판별기/prime-number-discriminator.java
+++ 240917/소수 판별기/prime-number-discriminator.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         boolean cd = true;
-        for (int i =2; i<=n; i++){
-            if (n % i == 0 && i != n){
+        for (int i =2; i<n; i++){
+            if (n % i == 0){
                 cd = false;
                 break;
             }
