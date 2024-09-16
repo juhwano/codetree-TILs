a6af7f803ada8bd31435a91cdc2fd346712c741c
JUHWAN OH <calculus9006@gmail.com>
2024-09-16 12:49:29 +0900

    Title: 코로나 메뉴얼, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240916/코로나 메뉴얼/covid-manual.java
+++ 240916/코로나 메뉴얼/covid-manual.java
@@ -1,33 +1,36 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        int imergence =0;
         Scanner sc = new Scanner(System.in);
-        char isColdA = sc.next().charAt(0);
-        int tempA = sc.nextInt();
 
-        char isColdB = sc.next().charAt(0);
-        int tempB = sc.nextInt();
+        // 변수 선언
+        char c1, c2, c3;
+        int t1, t2, t3;
+        
+        // 입력
+        c1 = sc.next().charAt(0);
+        t1 = sc.nextInt();
+        c2 = sc.next().charAt(0);
+        t2 = sc.nextInt();
+        c3 = sc.next().charAt(0);
+        t3 = sc.nextInt();
 
-        char isColdC = sc.next().charAt(0);
-        int tempC = sc.nextInt();
-
-        if (isColdA == 'Y' && tempA >= 37){
-            imergence += 1;
+        // 출력
+        // A가 2명 이상인지 판단하기
+        if(c1 == 'Y' && t1 >= 37) {
+            // 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+            if((c2 == 'Y' && t2 >= 37) || (c3 == 'Y' && t3 >= 37))
+                System.out.println("E");
+            else
+                System.out.println("N");
         }
-        if (isColdB == 'Y' && tempB >= 37){
-            imergence += 1;
+        else {
+            // 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+            if((c2 == 'Y' && t2 >= 37) && (c3 == 'Y' && t3 >= 37))
+                System.out.println("E");
+            else
+                System.out.println("N");
         }
-        if (isColdC == 'Y' && tempC >= 37){
-            imergence += 1;
-        }
-        if (imergence >= 2){
-            System.out.println("E");
-        }else{
-            System.out.println("N");
-        }
-        sc.close();
-
     }
 }
