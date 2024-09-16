40c9804c1a36af39ecbab751f8b3b651028dd4d5
JUHWAN OH <calculus9006@gmail.com>
2024-09-16 15:23:37 +0900

    Title: 2 또는 3의 배수, Time: 219ms, Memory: 10MB, Status: Passed - CodeTree

--- 240916/2 또는 3의 배수/multiple-of-2-or-3.java
+++ 240916/2 또는 3의 배수/multiple-of-2-or-3.java
@@ -6,9 +6,9 @@ public class Main {
         int n = sc.nextInt();
         for (int i =1; i<=n; i++){
             if (i % 2 == 0 || i % 3 == 0){
-                System.out.print(1 +" ");
+                System.out.print("1 ");
             }else{
-                System.out.print(0 +" ");
+                System.out.print("0 ");
             }
         }
         
