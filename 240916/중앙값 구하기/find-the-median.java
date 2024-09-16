9bfd5463b08ef311269fbb67e4cb3ba1beb5fae0
JUHWAN OH <calculus9006@gmail.com>
2024-09-16 13:21:57 +0900

    Title: 중앙값 구하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240916/중앙값 구하기/find-the-median.java
+++ 240916/중앙값 구하기/find-the-median.java
@@ -6,12 +6,24 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        if (a >= b && a <= c){
-            System.out.println(a);
-        }else if(b >= a && b <= c){
-            System.out.println(b);
+        if (a > b){
+            // a > b
+            if (c > a){
+                System.out.println(a);
+            }else if(b > c){
+                System.out.println(b);
+            }else{
+                System.out.println(c);
+            }
         }else{
-            System.out.println(c);
+            // b > a
+            if (c > b){
+                System.out.println(b);
+            }else if (a > c){
+                System.out.println(a);
+            }else{
+                System.out.println(c);
+            }
         }
         sc.close();
     }
