b07deb2968e1126a029d04ea71d2919ca6090884
JUHWAN OH <calculus9006@gmail.com>
2024-09-16 17:58:29 +0900

    Title: a의 b승, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240916/a의 b승/a-to-the-power-of-b.java
+++ 240916/a의 b승/a-to-the-power-of-b.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int prod = a;
-        for (int i =1; i<b; i++){
+        int prod = 1;
+        for (int i =1; i<=b; i++){
             prod *= a;
         }
         System.out.print(prod);
