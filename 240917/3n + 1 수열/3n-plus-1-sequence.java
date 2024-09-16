72813c4f6e4b81d739ae9051614e00eb029aa89d
JUHWAN OH <calculus9006@gmail.com>
2024-09-17 00:09:12 +0900

    Title: 3n + 1 수열, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240917/3n + 1 수열/3n-plus-1-sequence.java
+++ 240917/3n + 1 수열/3n-plus-1-sequence.java
@@ -6,16 +6,16 @@ public class Main {
         int n =sc.nextInt();
         int cnt = 0;
         while (true){
+            if (n == 1){
+                System.out.print(cnt);
+                break;
+            }
             if (n % 2 == 0){
                 n /= 2;
             }else{
                 n = n * 3 + 1;
             }
             cnt++;
-            if (n == 1){
-                System.out.print(cnt);
-                break;
-            }
         }
     }
 }
