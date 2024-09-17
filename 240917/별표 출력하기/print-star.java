1644d963f9591c9102315c6f225d1721d27d301e
JUHWAN OH <calculus9006@gmail.com>
2024-09-17 18:11:13 +0900

    Title: 별표 출력하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240917/별표 출력하기/print-star.java
+++ 240917/별표 출력하기/print-star.java
@@ -12,8 +12,8 @@ public class Main {
             System.out.println();
         }
 
-        for (int i =0; i <n; i++){
-            for (int j=0; j < n-i-1; j++){
+        for (int i =n-2; i >=0; i--){
+            for (int j=0; j <=i; j++){
                 System.out.print("* ");
             }
             System.out.println();
