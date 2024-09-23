aba0f1e8c24fcb6d5afa3de8d980f1590dd2bf36
JUHWAN OH <calculus9006@gmail.com>
2024-09-24 01:41:47 +0900

    Title: 체크 다이아몬드, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240924/체크 다이아몬드/check-diamond.java
+++ 240924/체크 다이아몬드/check-diamond.java
@@ -5,24 +5,22 @@ public class Main {
         Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
         
-        for (int i = 0; i < 2*n-1; i++){
-            if (i <n){
-            // 상단
+        for (int i = 0; i < n; i++){
                 for (int j=n-1-i; j>0; j--){
                     System.out.print(" ");
                 }
                 for (int j=0; j<i+1; j++){
                     System.out.print("* ");
                 }
-            }else{
-            //하단
-                for (int k=0; k<i-n+1; k++){
+                System.out.println();
+        }
+        for (int i = 1; i < n; i++){
+                for (int k=0; k<i; k++){
                     System.out.print(" ");
                 }
-                for (int k=9-i; k>0; k--){
+                for (int k=n-i; k>0; k--){
                     System.out.print("* ");
                 }
-            }
             System.out.println();
         }
     }
