20b3a736e4e8c586d7d1912db039d76bbd0b8b07
JUHWAN OH <calculus9006@gmail.com>
2024-09-28 03:34:46 +0900

    Title: 좌우반전시킨 정사각형, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240928/좌우반전시킨 정사각형/left-right-inverted-square.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++){
+            for (int j=i*n-i; j>=0; j-=i){
+                System.out.print((i+j) +" ");
+            }
+            // for (int j=0; j<i*n; j+=i){
+            //     System.out.print((i+j) +" ");
+            // }
+            System.out.println();
+        }
+
+    }
+}
