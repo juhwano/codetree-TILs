d2cee223808349ddb6a69a156462aab841d64971
JUHWAN OH <calculus9006@gmail.com>
2024-10-03 01:04:56 +0900

    Title: 3n + 1 수열 m번, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 241003/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
+++ 241003/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -4,8 +4,9 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc =new Scanner(System.in);
         int m = sc.nextInt();
-        int cnt =0;
+        
         for (int i=0; i<m; i++){
+            int cnt =0;
             int n = sc.nextInt();
             while (n!=1){
                 if (n%2==0){
