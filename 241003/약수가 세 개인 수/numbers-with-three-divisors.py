1b4a9388c9280a767eea8d64241000213efcb5c4
JUHWAN OH <calculus9006@gmail.com>
2024-10-03 02:12:38 +0900

    Title: 약수가 세 개인 수, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241003/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -0,0 +1,11 @@
+start_num, end_num = map(int, input().split())
+
+result=0
+for i in range(start_num, end_num+1):
+    cnt=0
+    for j in range(1, i+1):
+        if (i%j==0):
+            cnt+=1
+    if (cnt==3):
+        result+=1
+print(result)
