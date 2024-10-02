8526f326fb019d904ecb668e0e2bcf07efd59884
JUHWAN OH <calculus9006@gmail.com>
2024-10-03 02:30:49 +0900

    Title: 소수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241003/소수 구하기/get-prime.py
+++ 241003/소수 구하기/get-prime.py
@@ -5,7 +5,8 @@ for i in range(1,n+1):
     is_prime = True;
 
     for j in range(2, i):
-        if (i%j==0): is_prime=false
+        if (i%j==0): 
+            is_prime=false
 
     if(is_prime):
         print(i, end=" ")
