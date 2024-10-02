47d079ff23dfff2acbcee84e5f7fb6c01155f2c1
JUHWAN OH <calculus9006@gmail.com>
2024-10-03 03:54:21 +0900

    Title: 원소 10개의 합, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 241003/원소 10개의 합/sum-of-10-elements.py
+++ 241003/원소 10개의 합/sum-of-10-elements.py
@@ -1,1 +1,7 @@
-print(sum(list(map(int, input().split()))))
+# print(sum(list(map(int, input().split()))))
+
+li = list(map(int, input().split()))
+sum=0
+for elem in li:
+    sum += elem
+print(sum)
