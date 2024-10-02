847b7339c1f407096032b247c058daa7991f9943
JUHWAN OH <calculus9006@gmail.com>
2024-10-03 01:43:11 +0900

    Title: 완전수, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241003/완전수/perfect-number.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt=0;
+        //진약수와 합
+        for (int i=a; i<=b; i++){
+            int sum=0;
+            for (int j=1; j<i; j++){
+                if (i%j==0){
+                    sum+=j;
+                }
+            }
+            if (sum == i){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+        
+        
+        
+        
+
+    }
+}
