496eaa1098bfae20be866e96b3ef5808dc3f762c
JUHWAN OH <calculus9006@gmail.com>
2024-10-08 16:25:03 +0900

    Title: 일의 자리 배열, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241008/일의 자리 배열/array-with-units.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+
+        for (int i = 2; i <10; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        for (int j=0; j<arr.length; j++){
+            System.out.print(arr[j]%10 +" " );
+        }
+
+
+    }
+}
