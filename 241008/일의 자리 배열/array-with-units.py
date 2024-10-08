9ae1eba9a34d5f78b17ee61a709a6d494c2cf088
JUHWAN OH <calculus9006@gmail.com>
2024-10-08 16:27:18 +0900

    Title: 일의 자리 배열, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 241008/일의 자리 배열/array-with-units.py
+++ 241008/일의 자리 배열/array-with-units.py
@@ -1,5 +1,13 @@
+# pp, p = map(int, input().split())
+# print(pp,p, end=" ")
+# for _ in range(3,11):
+#     pp, p = p, pp+ p
+#     print(p%10, end=" ")
+
 pp, p = map(int, input().split())
-print(pp,p, end=" ")
-for _ in range(3,11):
-    pp, p = p, pp+ p
-    print(p%10, end=" ")
+arr=[pp,p]
+for _ in range(2,10):
+    pp, p = p, pp + p
+    arr.append(p)
+for i in arr:
+    print(i%10, end=" ")
