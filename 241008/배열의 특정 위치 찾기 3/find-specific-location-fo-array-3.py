0a1e40972ac9655fc3dfdecf5ecc9b55e25c79f2
JUHWAN OH <calculus9006@gmail.com>
2024-10-08 11:28:26 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 241008/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ 241008/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -3,6 +3,7 @@ arr = list(map(int, input().split()))
 sum=0
 for i in range(len(arr)):
     if arr[i] == 0:
+        sum = arr[i-1] + arr[i-2] + arr[i-3]
         break;
-    sum+=arr[i]
+    sum = arr[-1] + arr[-2] + arr[-3]
 print(sum)
